7ca6a5236c362d51bffcda91919a722ac56e6957
Matheus Poletto <motaviop@gmail.com>
2023-05-01 22:57:34 -0300

    Adicionado menu superior.

--- Frontend/tasker/src/app/app.module.ts
+++ Frontend/tasker/src/app/app.module.ts
@@ -8,10 +8,22 @@ import { PaginaComponent } from './componentes/pagina/pagina.component';
 import { DashboardComponent } from './paginas/dashboard/dashboard.component';
 import { ListarUsuariosComponent } from './paginas/usuarios/listar-usuarios/listar-usuarios.component';
 import { SubnivelMenuLateralComponent } from './componentes/menu-lateral/subnivel-menu-lateral.component';
+import { MenuSuperiorComponent } from './componentes/menu-superior/menu-superior.component';
+
+import { OverlayModule } from '@angular/cdk/overlay';
+import { CdkMenuModule } from '@angular/cdk/menu';
 
 @NgModule({
-    declarations: [AppComponent, MenuLateralComponent, PaginaComponent, DashboardComponent, ListarUsuariosComponent, SubnivelMenuLateralComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    declarations: [
+        AppComponent,
+        MenuLateralComponent,
+        PaginaComponent,
+        DashboardComponent,
+        ListarUsuariosComponent,
+        SubnivelMenuLateralComponent,
+        MenuSuperiorComponent,
+    ],
+    imports: [BrowserModule, AppRoutingModule, OverlayModule, CdkMenuModule],
     providers: [],
     bootstrap: [AppComponent],
 })
---
+++ Frontend/tasker/src/app/componentes/menu-superior/header-dummy-data.ts
@@ -0,0 +1,43 @@
+export const languages = [
+    {
+        language: 'Português (Brasil)',
+        flag: 'br',
+    },
+];
+
+export const notifications = [
+    {
+        icon: 'fas fa-cloud-download',
+        subject: 'Download complete',
+        description: 'Lorem ipsum bla bla',
+    },
+    {
+        icon: 'fas fa-cloud-upload',
+        subject: 'Upload complete',
+        description: 'Lorem ipsum bla bla',
+    },
+    {
+        icon: 'fas fa-trash',
+        subject: '350mb trash files',
+        description: 'Lorem ipsum bla bla',
+    },
+];
+
+export const userItems = [
+    {
+        icon: 'fas fa-user',
+        label: 'Profile',
+    },
+    {
+        icon: 'fas fa-cog',
+        label: 'Settings',
+    },
+    {
+        icon: 'fas fa-unlock-alt',
+        label: 'Lock screen',
+    },
+    {
+        icon: 'fas fa-power-off',
+        label: 'Logout',
+    },
+];
